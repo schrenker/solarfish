0ba91029382cf795bf951dcb7894f37a8f408941
Bryan Chen <bryanchen74@gmail.com>
2021-07-10 16:53:41 -0400

    Remove fish_greeting.fish

--- fish_greeting.fish
+++
@@ -1,8 +0,0 @@
-function fish_greeting -d "What's up, fish?"
-    set_color normal
-    if test -n (echo (command -v cowsay))
-        cowsay "Welcome to fish, the friendly interactive shell"
-    else
-        echo $fish_greeting
-    end
-end
